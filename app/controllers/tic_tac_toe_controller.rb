e0b5b324c06d3fa177c471201ff1e276ce6a2752
Kaiyi Li <kaiyi4@gmail.com>
2016-07-28 18:51:16 -0700

    test endpoint

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -1,10 +1,10 @@
 class TicTacToeController < ApplicationController
   def get
-    b = Board.new()
-    b.player1 = 'kaiyi'
-    b.player2 = 'testRobot'
+    # b = Board.new()
+    # b.player1 = 'kaiyi'
+    # b.player2 = 'testRobot'
 
-    response = { :response_type => 'in_channel', :text => 'New game', :attachment => [ b ] }
+    response = { :response_type => 'in_channel', :text => 'New game' }
     render json: response
   end
 end
