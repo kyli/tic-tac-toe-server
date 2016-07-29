29cd25c052880c9127c1b70aa8b32aeea75e4e8d
Kaiyi Li <kaiyi4@gmail.com>
2016-07-28 23:11:44 -0700

    test endpoint

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -12,30 +12,52 @@ class TicTacToeController < ApplicationController
     end
 
     channel = params['channel_id']
-    existing = Board.find_by(:channel => channel)
-    if existing && existing.next then
-      return render json: { :text => 'There is one game per channel at a time' }
-    end
-
-    if existing then
-      existing.destroy
-    end
-
     userName = params['user_name']
     text = params['text']
-
     if !userName || !text then
       return render json: { :text => 'Bad command. /newgame @username' }
     end
 
-    opponent = /(@)(.*)/.match(text.strip)[2]
-    newgame = Board.new(:channel => channel, :player1 => userName, :player2 => opponent, :state => '000000000', :next => userName)
-    if newgame.save() then
-      response = { :response_type => 'in_channel', :text => 'New game created for ' + userName + ' and ' + opponent + '. @' + userName + '\'s move'}
-      render json: response
-    else
-      render json: { :text => 'Something happened, try again' }
+    existing = Board.find_by(:channel => channel)
+    if existing then
+      if existing.next then
+        return render json: { :text => 'There is one game per channel at a time' }
+      else
+        existing.destroy
+      end
     end
+
+    opponent = /(@)(.*)/.match(text.strip)[2]
+    newgame = Board.new(:channel => channel,
+                        :player1 => userName,
+                        :player2 => opponent,
+                        :state => '000000000',
+                        :next => userName)
+    if newgame.save() then
+      render json: { :response_type => 'in_channel', :text => 'New game created for ' + userName + ' and ' + opponent + '. @' + userName + '\'s move'}
+    else
+      render json: { :text => 'Something error happened, try again' }
+    end
+  end
+
+  def get
+    if not checkToken(params['token'], 'pqNgvumzhx5SO6VWqd8YeShN') then
+      return render json: { :text => 'Incorrect token' }, :status => 400
+    end
+    channel = params['channel_id']
+
+    existing = Board.find_by(:channel => channel)
+    if existing then
+      if !existing.next then
+        return render json: { :text => 'The current game is complete between ' + existing.player1 + ' and ' + existing.player2,
+                              :attachments => [ existing.state ] }
+      else
+        return render json: { :text => 'The current game is ongoing between ' + existing.player1 + ' and ' + existing.player2 + '. ' + existing.next + '\'s move',
+                              :attachments => [ existing.state ] }
+      end
+    end
+
+    return render json: { :text => 'No games in this channel.' }
   end
 
   def del
@@ -49,21 +71,5 @@ class TicTacToeController < ApplicationController
       existing.destroy
       return render json: { :text => 'Removed the current game for the channel. It was between ' + existing.player1 + " and " + existing.player2 }
     end
-  end
-
-  def get
-    if not checkToken(params['token'], 'pqNgvumzhx5SO6VWqd8YeShN') then
-      return render json: { :text => 'Incorrect token' }, :status => 400
-    end
-    channel = params['channel_id']
-    userName = params['user_name']
-    text = params['text']
-    b = Board.new(:channel => channel)
-    b.player1 = userName
-    b.player2 = 'testRobot'
-    b.state = '001012200'
-    b.next = userName
-    response = { :response_type => 'in_channel', :text => 'New game for ' + userName, :attachments => [ b ] }
-    render json: response
   end
 end
