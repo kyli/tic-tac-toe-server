a46984e221b8ec2418524087ea49a9655e207efb
Kaiyi Li <kaiyi4@gmail.com>
2016-07-29 00:29:47 -0700

    test endpoint

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -18,7 +18,7 @@ class TicTacToeController < ApplicationController
                      when '2' then ' O '
                    end
       end
-      result += '] , \\\n'
+      result += '] , &lt;br&gt;'
     end
     return result
   end
